30ea408876742df17bd2af311c6d78b87f9c02a2
Seth Fischer <seth@fischer.nz>
2022-11-05 16:04:28 +1300

    refactor: build release

--- src/osr_mechanical/console/application.py
+++ src/osr_mechanical/console/application.py
@@ -3,42 +3,33 @@
 import csv
 import importlib
 import logging
-import tarfile
 from argparse import ArgumentParser, Namespace
 from base64 import b64encode
 from datetime import datetime
 from json import JSONDecodeError
 from os import EX_OK, getcwd
 from pathlib import Path
-from shutil import rmtree
-from subprocess import run
 from sys import stdout
-from zipfile import ZIP_DEFLATED, ZipFile
 
-from cadquery import exporters
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 from osr_mechanical import __version__
 from osr_mechanical.bom.bom import Bom
 from osr_mechanical.config import (
     COPYRIGHT_OWNER,
-    DOCUMENTATION_URL,
     PROJECT_HOST,
     PROJECT_NAME,
-    PROJECT_URL,
-    REPO_URL,
     SHORT_DESCRIPTION,
 )
 from osr_mechanical.console.dxf import dxf_import_export
 from osr_mechanical.console.exporters import ExportPNG
-from osr_mechanical.final import FinalAssembly
-from osr_mechanical.jigs.vslot import EndTapJig
+from osr_mechanical.console.release import ReleaseBuilder
 
 logging.basicConfig(encoding="utf-8", level=logging.INFO)
 logger = logging.getLogger("osr_mechanical.console")
 
 
-def export_png_cmd(args: Namespace) -> None:
+def export_png(args: Namespace) -> None:
     """Export PNG image of final assembly."""
     logger.debug("Exporting final assembly PNG.")
 
@@ -51,7 +42,7 @@ def export_png_cmd(args: Namespace) -> None:
     exit(EX_OK)
 
 
-def create_open_graph_card_svg(args: Namespace) -> None:
+def open_graph_card_svg(args: Namespace) -> None:
     """Create Open Graph Card in SVG format."""
     env = Environment(
         loader=PackageLoader("osr_mechanical"), autoescape=select_autoescape()
@@ -89,131 +80,14 @@ def create_open_graph_card_svg(args: Namespace) -> None:
     exit(EX_OK)
 
 
-def get_release_name(project_name: str, version: str) -> str:
-    """Release name."""
-    return f"{project_name}-cam-{version}"
-
-
-def export_jigs(out_directory: Path) -> None:
-    """Export jigs as STL for 3D printing."""
-    logger.debug("Exporting jigs.")
-
-    out_directory.mkdir()
-
-    end_tap_jig_pathname = out_directory / "vslot-end-tap-jig-2020.stl"
-    end_tap_jig = EndTapJig(simple=True)
-    exporters.export(
-        end_tap_jig.cq_part("2020_end_tap_jig__body"),
-        str(end_tap_jig_pathname),
-    )
-
-
-def export_final_assembly_step(out_file: Path) -> None:
-    """Export final assembly as STEP."""
-    logger.debug("Exporting final assembly STEP.")
-
-    exporters.export(
-        FinalAssembly().cq_object.toCompound(),
-        str(out_file),
-        tolerance=0.01,
-        angularTolerance=0.1,
-    )
-
-
-def export_final_assembly_png(out_file: Path) -> None:
-    """Export PNG image of final assembly for release archive."""
-    logger.debug("Exporting final assembly PNG of release archive.")
-
-    exporter = ExportPNG(out_file)
-    exporter.export()
-
-
-def create_readme(out_file: Path) -> int:
-    """Create readme file."""
-    logger.debug("Creating readme file.")
-
-    env = Environment(
-        loader=PackageLoader("osr_mechanical"), autoescape=select_autoescape()
-    )
-
-    now = datetime.utcnow()
-
-    template = env.get_template("README.md")
-    result = template.render(
-        build_time=now,
-        copyright_owner=COPYRIGHT_OWNER,
-        documentation_url=DOCUMENTATION_URL,
-        project_repo_url=REPO_URL,
-        project_url=PROJECT_URL,
-        short_description=SHORT_DESCRIPTION,
-        version=__version__,
-    )
-
-    return out_file.write_text(result)
-
-
-def export_changelog(out_file: Path) -> int:
-    """Export changelog."""
-    logger.debug("Exporting changelog.")
-
-    result = run(
-        ["cz", "changelog", "--file-name", out_file],
-        capture_output=True,
-        text=True,
-    )
-    result.check_returncode()
-
-    return result.returncode
-
-
-def tar_directory(directory: Path, out_file: Path, arcname: str):
-    """Create tar archive of a directory."""
-    logger.debug(f"Creating tar archive {out_file}.")
-    with tarfile.open(out_file, "w:gz") as tar:
-        tar.add(directory, arcname=arcname)
-
-
-def zip_directory(directory: Path, out_file: Path, arcname: str):
-    """Create zip archive of directory."""
-    logger.debug(f"Creating zip archive {out_file}.")
-    with ZipFile(out_file, "w", ZIP_DEFLATED) as zip_file:
-        for entry in directory.rglob("*"):
-            zip_file.write(entry, Path(arcname / entry.relative_to(directory)))
-
-
 def build_cam_archive(args: Namespace) -> None:
     """Build Computer Aided Manufacturing file archive."""
     if not args.build_dir.is_dir():
         logger.critical(f"Build directory does not exist {args.build_dir}.")
         exit(1)
 
-    release_name = get_release_name(PROJECT_NAME, __version__)
-    release_directory = args.build_dir / release_name
-
-    if release_directory.is_dir():
-        logger.info(f"Removing release directory {release_directory}.")
-        rmtree(release_directory)
-
-    release_directory.mkdir()
-
-    create_readme(release_directory / "README.md")
-    export_changelog(release_directory / "CHANGELOG.md")
-    export_final_assembly_step(release_directory / f"{PROJECT_NAME}.step")
-    export_final_assembly_png(release_directory / f"{PROJECT_NAME}.png")
-    export_jigs(release_directory / "jigs")
-    export_bom(release_directory / "bom")
-
-    tar_directory(
-        release_directory,
-        args.build_dir / f"{release_name}.tar.gz",
-        release_name,
-    )
-
-    zip_directory(
-        release_directory,
-        args.build_dir / f"{release_name}.zip",
-        release_name,
-    )
+    builder = ReleaseBuilder(args.build_dir)
+    builder.build()
 
     exit(EX_OK)
 
@@ -222,30 +96,10 @@ def dxf_reduce(args: Namespace) -> None:
     """Import a DXF followed by export."""
     output = dxf_import_export(args.filename)
     stdout.write(output)
-    exit(1)
+    exit(EX_OK)
 
 
-def export_bom(out_directory: Path) -> None:
-    """Export bill of materials."""
-    logger.debug("Exporting bill of materials.")
-
-    out_directory.mkdir()
-    csv_pathname = out_directory / "bom.csv"
-
-    final = FinalAssembly()
-    jig_end_tap = EndTapJig()
-
-    bom = Bom()
-    bom.insert_assembly(final.cq_object)
-    bom.insert_assembly(jig_end_tap.cq_object)
-
-    data = bom.encode(encoder=Bom.ENCODE_CSV)
-
-    with open(csv_pathname, mode="w") as f:
-        f.write(data)
-
-
-def generate_bom(args: Namespace) -> None:
+def export_bom(args: Namespace) -> None:
     """Generate bill of materials."""
     module_name, class_name = f"osr_mechanical.{args.assembly}".rsplit(".", 1)
 
@@ -349,13 +203,13 @@ def build_parser() -> ArgumentParser:
         nargs=1,
         help="output file",
     )
-    parser_export_png.set_defaults(func=export_png_cmd)
+    parser_export_png.set_defaults(func=export_png)
 
     parser_open_graph_card = subparsers.add_parser(
         "open-graph-card",
         help="create open graph card SVG",
     )
-    parser_open_graph_card.set_defaults(func=create_open_graph_card_svg)
+    parser_open_graph_card.set_defaults(func=open_graph_card_svg)
 
     parser_bom = subparsers.add_parser("bom", help="generate bill of materials")
     parser_bom.add_argument(
@@ -370,7 +224,7 @@ def build_parser() -> ArgumentParser:
         default="final.FinalAssembly",
         help="assembly for which to generate bill of materials",
     )
-    parser_bom.set_defaults(func=generate_bom)
+    parser_bom.set_defaults(func=export_bom)
 
     return parser
 
---
+++ src/osr_mechanical/console/release.py
@@ -0,0 +1,166 @@
+"""Release builder."""
+
+import tarfile
+from datetime import datetime
+from pathlib import Path
+from shutil import rmtree
+from subprocess import run
+from zipfile import ZIP_DEFLATED, ZipFile
+
+from cadquery import exporters
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+from osr_mechanical import __version__
+from osr_mechanical import __version__ as project_version
+from osr_mechanical.bom.bom import Bom
+from osr_mechanical.config import (
+    COPYRIGHT_OWNER,
+    DOCUMENTATION_URL,
+    PROJECT_NAME,
+    PROJECT_URL,
+    REPO_URL,
+    SHORT_DESCRIPTION,
+)
+from osr_mechanical.console.exporters import ExportPNG
+from osr_mechanical.final import FinalAssembly
+from osr_mechanical.jigs.vslot import EndTapJig
+
+
+class ReleaseBuilder:
+    """Build Computer Aided Manufacturing file archive."""
+
+    def __init__(self, build_directory: Path):
+        """Initialise."""
+        self.build_directory = build_directory
+        self.release_name = f"{PROJECT_NAME}-cam-{project_version}"
+        self.release_directory = self.build_directory / self.release_name
+
+        if not self.build_directory.is_dir():
+            raise FileNotFoundError(
+                f"Build directory '{self.build_directory}' does not exist."
+            )
+
+    def build(self):
+        """Build release."""
+        self.remove_directory(self.release_directory)
+        self.release_directory.mkdir()
+
+        self.readme(self.release_directory / "README.md")
+        self.changelog(self.release_directory / "CHANGELOG.md")
+        self.final_assembly_step(self.release_directory / f"{PROJECT_NAME}.step")
+        self.final_assembly_png(self.release_directory / f"{PROJECT_NAME}.png")
+        self.jigs(self.release_directory / "jigs")
+        self.bom(self.release_directory / "bom")
+        self.archive()
+
+    @staticmethod
+    def remove_directory(release_directory: Path) -> None:
+        """Remove release directory."""
+        if release_directory.is_dir():
+            rmtree(release_directory)
+
+    @staticmethod
+    def readme(out_file: Path) -> int:
+        """Create README file."""
+        env = Environment(
+            loader=PackageLoader("osr_mechanical"),
+            autoescape=select_autoescape(),
+        )
+
+        template = env.get_template("README.md")
+        result = template.render(
+            build_time=datetime.utcnow(),
+            copyright_owner=COPYRIGHT_OWNER,
+            documentation_url=DOCUMENTATION_URL,
+            project_repo_url=REPO_URL,
+            project_url=PROJECT_URL,
+            short_description=SHORT_DESCRIPTION,
+            version=__version__,
+        )
+
+        return out_file.write_text(result)
+
+    @staticmethod
+    def changelog(out_file: Path) -> int:
+        """Create changelog."""
+        result = run(
+            ["cz", "changelog", "--file-name", out_file],
+            capture_output=True,
+            text=True,
+        )
+        result.check_returncode()
+
+        return result.returncode
+
+    @staticmethod
+    def final_assembly_step(out_file: Path) -> None:
+        """Export final assembly as STEP."""
+        exporters.export(
+            FinalAssembly().cq_object.toCompound(),
+            str(out_file),
+            tolerance=0.01,
+            angularTolerance=0.1,
+        )
+
+    @staticmethod
+    def final_assembly_png(out_file: Path) -> None:
+        """Export PNG image of final assembly for release archive."""
+        exporter = ExportPNG(out_file)
+        exporter.export()
+
+    @staticmethod
+    def jigs(out_directory: Path) -> None:
+        """Export jigs as STL for 3D printing."""
+        out_directory.mkdir()
+
+        end_tap_jig_pathname = out_directory / "vslot-end-tap-jig-2020.stl"
+        end_tap_jig = EndTapJig(simple=True)
+        exporters.export(
+            end_tap_jig.cq_part("2020_end_tap_jig__body"),
+            str(end_tap_jig_pathname),
+        )
+
+    @staticmethod
+    def bom(out_directory: Path) -> None:
+        """Export bill of materials."""
+        out_directory.mkdir()
+        csv_pathname = out_directory / "bom.csv"
+
+        final = FinalAssembly()
+        jig_end_tap = EndTapJig()
+
+        bom = Bom()
+        bom.insert_assembly(final.cq_object)
+        bom.insert_assembly(jig_end_tap.cq_object)
+
+        data = bom.encode(encoder=Bom.ENCODE_CSV)
+
+        with open(csv_pathname, mode="w") as f:
+            f.write(data)
+
+    def archive(self):
+        """Create release archives."""
+        self.tar(
+            self.release_directory,
+            self.build_directory / f"{self.release_name}.tar.gz",
+            self.release_name,
+        )
+
+        self.zip(
+            self.release_directory,
+            self.build_directory / f"{self.release_name}.zip",
+            self.release_name,
+        )
+
+    @staticmethod
+    def tar(directory: Path, out_file: Path, arcname: str):
+        """Create tar archive of release."""
+        with tarfile.open(out_file, "w:gz") as tar:
+            tar.add(directory, arcname=arcname)
+
+    @staticmethod
+    def zip(directory: Path, out_file: Path, arcname: str):
+        """Create zip archive of release."""
+        with ZipFile(out_file, "w", ZIP_DEFLATED) as zip_file:
+            for entry in directory.rglob("*"):
+                zip_file.write(entry, Path(arcname / entry.relative_to(directory)))
