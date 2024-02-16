6e6cab9b06079c513683ea1ae5a8ab7ba1b25930
Seth Fischer <seth@fischer.nz>
2024-02-16 17:00:58 +1300

    chore: set mwg image tags

--- image-meta.sh
+++ image-meta.sh
@@ -8,21 +8,25 @@ fi
 image_file=$1
 
 copyright_owner="Seth Fischer"
+today=$(date +%Y-%m-%d)
 current_year=$(date +%Y)
-copyright_notice="(c) ${current_year} ${copyright_owner}; Licence: MIT License"
+licence="MIT License"
+licence_url="https://spdx.org/licenses/MIT.html"
+copyright_notice="(c) ${current_year} ${copyright_owner}; Licence: ${licence}"
 project_url="https://rover.fischer.nz"
 description="Final assembly of sethfischer-rover, a quarter-scale Mars rover. See <${project_url}>. Based on NASA-JPL's Perseverance Mars Rover."
 
 exiftool \
     -overwrite_original \
-    -Artist="${copyright_owner}" \
-    -Copyright="${copyright_notice}" \
     -AttributionName="${copyright_owner}" \
     -AttributionURL="${project_url}" \
-    -Comment="Alternative body for the NASA JPL Open Source Rover." \
-    -Description="${description}" \
-    -License="https://spdx.org/licenses/MIT.html" \
-    -Rights="MIT License" \
-    -UsageTerms="MIT License" \
+    -Comment="${description}" \
+    -License="${licence_url}" \
+    -MWG:Copyright="${copyright_notice}" \
+    -MWG:CreateDate="${today}" \
+    -MWG:Creator="${copyright_owner}" \
+    -MWG:DateTimeOriginal="${today}" \
+    -MWG:Description="${description}" \
     -Software="${project_url}" \
+    -UsageTerms="${licence}" \
     "$image_file"
