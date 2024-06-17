03dc7d7126497966f91e1baf133aff260307ab21
Seth Fischer <seth@fischer.nz>
2024-06-17 20:40:16 +1200

    docs: vmc daughter board directed graph

--- src/osr_elec/pinout/vmc_header/diagram.py
+++ src/osr_elec/pinout/vmc_header/diagram.py
@@ -8,14 +8,15 @@ from pinout.core import Group, Image
 # this is a collection of definition files rather than a Python module
 import data  # type: ignore[import-not-found] # isort: skip
 
-diagram = Diagram_2Rows(685, 730, 650, "diagram")
+diagram = Diagram_2Rows(710, 730, 650, "diagram")
 diagram.add_stylesheet("styles.css", embed=True)
 
-graphic = diagram.panel_01.add(Group(310, 30))
+graphic = diagram.panel_01.add(Group(322, 30))
 
 hardware = graphic.add(Image("pin-header-2x20-p2.54mm-vertical.svg", embed=True))
 hardware.add_coord("pin_1", 60, 15)
 hardware.add_coord("pin_2", 0, 15)
+hardware.add_coord("spi_to_uart", 60, 0)
 hardware.add_coord("pin_pitch_v", 0, 30)
 hardware.add_coord("pin_pitch_h", 30, 0)
 
